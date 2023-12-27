445f644922ceea8e2130c73b95d222e7ad1521c4
ASLIKIZILASLAN <104174681+ASLIKIZILASLAN@users.noreply.github.com>
2023-12-27 10:51:01 +0300

    Create  CategoryViewModel.cs

---
+++ CategoryViewModel.cs
@@ -0,0 +1,10 @@
+namespace WebMVC.Models
+{
+    public class CategoryViewModel
+    {
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int BlogCount { get; set; }
+    }
+}
+CategoryViewModel.cs
