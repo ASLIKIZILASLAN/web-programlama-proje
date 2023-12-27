2b377da93508b66dfdeca2fadc58f89a919a6c25
ASLIKIZILASLAN <104174681+ASLIKIZILASLAN@users.noreply.github.com>
2023-12-27 10:49:53 +0300

    Create  CategoryListModel.cs

---
+++ CategoryListModel.cs
@@ -0,0 +1,10 @@
+namespace WebMVC.Models
+{
+    public class CategoryListModel
+    {
+        public string CategoryID { get; set; }
+        public string  CategoryName { get; set; }
+        public List<CategoryViewModel> CategoryList { get; set; }
+    }
+}
+CategoryListModel.cs
